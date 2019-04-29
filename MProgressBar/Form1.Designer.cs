c53cd203ad46bbfc1a2ad1ddabefaa38d9f21aa3
michalkl <michalkl@mikronika.com.pl>
2019-04-29 15:27:39 +0200

    Add project files.

---
+++ MProgressBar/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace MProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pb
+            // 
+            this.pb.Location = new System.Drawing.Point(12, 12);
+            this.pb.MarqueeAnimationSpeed = 90;
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(310, 48);
+            this.pb.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.pb.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 50);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "asdasdasdasdadsasdasdasd";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(310, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 161);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pb);
+            this.Name = "Form1";
+            this.Text = "Progress";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MProgressBar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MProgressBar
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            System.Threading.Tasks.Task pgBar = Task.Run(() =>
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new CancellableProgressBar());
+            });
+            while(true)
+            {
+                Console.WriteLine("HEllo there");
+                System.Threading.Thread.Sleep(1000);
+            }
+            
+        }
+    }
+}
